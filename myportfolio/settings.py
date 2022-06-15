f10644aa01b3bcc761f8f072c4c3cad709c813d1
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-15 11:35:32 +0300

    Adding API Endpoints

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+#
 DATABASES = {
     'default': {
      'ENGINE': 'django.db.backends.postgresql',
